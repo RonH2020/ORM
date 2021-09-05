7002d3184ce244fc727e7d17294a46b1060ff53a
Ronald Holden <rholden.rh@gmail.com>
2021-09-05 18:09:34 -0500

    finished the definitions of the columns. Working on the api routes.

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  try {
+    const categoryData = await Category.findAll({
+      //include its associated Products
+      include: [{ model: Product }], 
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      //include its associated Products
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const locationData = await Category.create({
+      product_id: req.body.product_id,
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
